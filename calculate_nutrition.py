03f137ccf1ba103062c0aac6dcbcf35801546023
Duy Vu <duy.vu@tuni.fi>
2021-03-16 18:11:23 +0200

    Make app running

--- calculate_nutrition.py
+++ calculate_nutrition.py
@@ -1,6 +1,5 @@
 # TIE-02107 Introduction to Programming
 # Student name: Duy Vu
-# Student number: 292118
 # Nutrition calculator
 
 #Normally, on the back label of food, the nutritional value(carbonhydrat, protein,
@@ -23,32 +22,33 @@
 #After having the result, you also have an option of exporting data to a csv
 #file to keep track of nutrition value per meal
 
-from utils import *
+import utils
+import pandas as pd
+from tkinter import *
 
 LABEL_NAME = ['Carb (g)', 'Protein (g)', 'Fat (g)', 'Fiber (g)', 'Calories (kcal)']
 
 class AppGUI:
     def __init__(self):
-        self.__mainwindow = tk.Tk()
-        self.__dataframe = read_data('df.parquet')
+        self.__mainwindow = Tk()
+        self.__column_names = ['Name'] + LABEL_NAME
+        self.__dataframe = utils.read_data(self.__column_names)
         self.__nutrition_value = []
-        self.__stored_result = []
         
         # Entry values
-        self.__food_name = tk.Entry(self.__mainwindow, width=15)
-        for i in range(len(LABEL_NAME)):
-            value_label = tk.Entry(self.__mainwindow, width=15)
+        self.__food_name = Entry(self.__mainwindow, width=15)
+        for _ in range(len(LABEL_NAME)):
+            value_label = Entry(self.__mainwindow, width=15)
             self.__nutrition_value.append(value_label)
-        self.__amount = tk.Entry(self.__mainwindow, width=10)
-        self.__serving_num = tk.Entry(self.__mainwindow, width=10)
-        self.__calculate_button = tk.Button(self.__mainwindow, text = "Nutriontion in 1 serving of food", 
-                                                            bg="aqua", command = self.calculate_nutrition)
+        self.__amount = Entry(self.__mainwindow, width=10)
+        self.__serving_num = Entry(self.__mainwindow, width=10)
+        self.__calculate_button = Button(self.__mainwindow, 
+                                            text = "Nutriontion in 1 serving of food", 
+                                            bg="aqua", 
+                                            command = self.calculate_nutrition)
         self.__result_ui = []
-        self.__stop_button = tk.Button(self.__mainwindow, text = "QUIT!", command=self.quit)
+        self.__stop_button = Button(self.__mainwindow, text = "QUIT!", command=self.quit)
         self.create_ui()
-
-        # Overriding Tkinter “X” button control (close the window) 
-        self.protocol("WM_DELETE_WINDOW", self.on_exit)
 
     def create_ui(self):
         """
@@ -57,32 +57,35 @@ class AppGUI:
 
         # Title and headings
         self.__mainwindow.title("Nutrion calculator")
-        tk.Label(self.__mainwindow, text='Name of food').grid(row=0, column=0, sticky=tk.W)
+        Label(self.__mainwindow, text='Name of food').grid(row=0, column=0, sticky=W)
         self.__food_name.grid(row=0, column=1)
-        tk.Label(self.__mainwindow, text = "Nutrion in 100 grams of food").grid(row=1, columnspan = 5, sticky=tk.W+tk.E)
+        Label(self.__mainwindow, text = "Nutrion in 100 grams of food") \
+            .grid(row=1, columnspan = 5, sticky=W+E)
 
         # Nutrition values
         for i in range(len(LABEL_NAME)):
-            tk.Label(self.__mainwindow, text=LABEL_NAME[i]).grid(row=2, column=i)
+            Label(self.__mainwindow, text=LABEL_NAME[i]).grid(row=2, column=i)
             self.__nutrition_value[i].grid(row=3, column=i)
 
         # Label for the amount of food and numbers of servings/meals
-        tk.Label(self.__mainwindow, text="Amount of food in 1 package (g)").grid(row=4, column=0, columnspan=2, sticky=tk.W)
-        self.__amount.grid(row=4, column=2, sticky=tk.E)
-        tk.Label(self.__mainwindow, text="Numbers of meals you want to eat").grid(row=5, column=0, columnspan=3, sticky=tk.W)
-        self.__serving_num.grid(row=5, column=2, sticky=tk.E)
+        Label(self.__mainwindow, text="Amount of food in 1 package (g)") \
+            .grid(row=4, column=0, columnspan=2, sticky=W)
+        self.__amount.grid(row=4, column=2, sticky=E)
+        Label(self.__mainwindow, text="Numbers of meals you want to eat") \
+            .grid(row=5, column=0, columnspan=3, sticky=W)
+        self.__serving_num.grid(row=5, column=2, sticky=E)
 
         # Calculation button
-        self.__calculate_button.grid(row=6, columnspan=2, sticky=tk.W)
+        self.__calculate_button.grid(row=6, columnspan=2, sticky=W)
 
         # Label of results
         for i in range(len(LABEL_NAME)):
-            result_ui = tk.Label(self.__mainwindow)
+            result_ui = Label(self.__mainwindow)
             result_ui.grid(row=7, column=i)
             self.__result_ui.append(result_ui)
 
         #Explaination text
-        self.__explanation_text = tk.Label(self.__mainwindow)
+        self.__explanation_text = Label(self.__mainwindow)
         self.__explanation_text.grid(row=7, columnspan=2)
 
         # Quit button
@@ -90,35 +93,40 @@ class AppGUI:
 
     def calculate_nutrition(self):
         """
+        Calculate nutriition per meal 
+        and print them on the UI
         """
 
-        amount = self.check_valid_input(self.__amount, self.__explanation_text)
-        serving = self.check_valid_input(self.__serving_num, self.__explanation_text, zero_value=True)
-        float_value = [self.check_valid_input(value, self.__explanation_text) for value in self.__nutrition_value]
+        amount = utils.check_valid_input(self.__amount, self.__explanation_text)
+        serving = utils.check_valid_input(self.__serving_num, self.__explanation_text, zero_value=True)
+        float_value = [utils.check_valid_input(value, self.__explanation_text) for value in self.__nutrition_value]
         
-        if INVALID_INPUT in (amount, serving) or INVALID_INPUT in float_value:
+        if utils.INVALID_INPUT in (amount, serving) or utils.INVALID_INPUT in float_value:
             self.delete_results()
             return
 
         serving_amount = amount / serving  # mass for 1 serving
         serving_ratio = 100 / serving_amount
 
+        # Print results and store them
         self.__explanation_text.configure(text="")   
-  
+        row_data = [str(self.__food_name.get())]
         for i in range(len(self.__nutrition_value)):
-            result = "{:.1f}".format(float_value[i]/serving_ratio)
-            self.__result_ui[i].configure(text=result)
-            self.__stored_result.append(result)
-            
+            result = round(float_value[i]/serving_ratio, 1)
+            self.__result_ui[i].configure(text=str(result))
+            row_data.append(result)
+
+        row_df = pd.DataFrame([row_data], columns=self.__column_names)
+        self.__dataframe = pd.concat([self.__dataframe, row_df], ignore_index=True)
+        
         # Export data if needed
-        export_button = tk.Button(self.__mainwindow, text='Export data', command=self.export_data)
-        export_button.grid(row=8, column=0, columnspan=2, sticky=tk.W)
+        export_button = Button(self.__mainwindow, text='Export data', command=self.export_data)
+        export_button.grid(row=8, column=0, columnspan=2, sticky=W)
 
     def delete_results(self):
         """ 
-        In error situations this method will delete self.__result_ui
+        Clear all entries
         """
-        
         for i in range(len(self.__result_ui)):
             self.__result_ui[i].configure(text="")
 
@@ -126,8 +134,7 @@ class AppGUI:
         """
         Export data to file
         """
-
-        # self.__dataframe.to_csv("Nutrition.csv")
+        self.__dataframe.to_csv("Nutrition.csv", sep=',')
         self.delete_results()
         self.__explanation_text.configure(text="Your data is exported successfully!")
 
@@ -142,13 +149,12 @@ class AppGUI:
         Store data in parquet file with file's name and compression type decided by user 
         and end the execution of the program.
         """
-        other_params = {'compression': 'snappy'} # default
-        store_data("user_data.parquet", **other_params)
+        self.__dataframe.to_csv("Nutrition.csv", index=False)
         self.__mainwindow.destroy()
 
 
 def main():
     ui = AppGUI()
-    ui.start()
+    ui.start() 
 
 main()
--- utils.py
+++ utils.py
@@ -1,12 +1,13 @@
-import tkinter as tk
-import numpy as np
+from tkinter import *
 import pandas as pd
-import pyarrow.parquet as pq
 
 INVALID_INPUT = None
 
 def check_valid_input(entry, explanation_text, zero_value=False):
     """
+    Check if input value is valid or not.
+    In other words, all entries must only accept
+    non-negative number, 
     """
 
     try: 
@@ -14,43 +15,29 @@ def check_valid_input(entry, explanation_text, zero_value=False):
 
         if entry_num < 0:
             explanation_text.configure(text="Please type in positive numbers!")
-            entry.delete(0, tk.END)
+            entry.delete(0, END)
             return INVALID_INPUT
 
         if zero_value and (entry_num == 0):
-            explanation_text.configure(text="The numbers of serving must be larger than zero")
-            entry.delete(0, tk.END)
+            explanation_text.configure(text="Number of meals must not be 0!")
+            entry.delete(0, END)
             return INVALID_INPUT
 
         return entry_num
 
     except ValueError:
         explanation_text.configure(text="Please type in number only!")
-        entry.delete(0, tk.END)
+        entry.delete(0, END)
         return INVALID_INPUT
 
 
-def read_data(file_in, columns_name):
+def read_data(columns_name):
     """
+    Read csv file containing user info
     """
-    
     try: 
-        df = pq.read_table(file_in) 
+        df = pd.read_csv("Nutrition.csv", sep=',', index_col=False) 
     except OSError:
-        df = pd.DataFrame(columns=columns_name, dtype=np.float32)
+        df = pd.DataFrame(columns=columns_name)
+
     return df
-
-
-def store_data(file_out, **kwargs):
-    """
-    """
-
-    table = pq.read_table(file_out, **kwargs)
-    table.to_pandas()
-
-
-def export_data(data_frame, file_name):
-    """
-    """
-    
-    data_frame.to_csv(file_name)
